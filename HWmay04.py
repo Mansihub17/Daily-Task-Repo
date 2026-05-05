bd7f1f3887b3b782c869d047bf493f9e7255c9d8
Mansihub17 <mansirahangdale90@gmail.com>
2026-05-05 14:57:52 +0530

    mini project on MRP

---
+++ HWmay04.py
@@ -0,0 +1,6 @@
+MRP = 110399
+print ("MRP of the product is:", MRP)
+Discount = 34/100 * MRP
+print("Discount on the product is:", Discount)
+Selling_price = 72990
+print("Selling price of the product is:", Selling_price)
