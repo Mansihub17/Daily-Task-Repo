f7dd2ad9bec5c32b35271a2c20b8e87de07ef26a
Mansihub17 <mansirahangdale90@gmail.com>
2026-05-13 15:37:30 +0530

    conditional statement task

---
+++ Taskmay12.py
@@ -0,0 +1,9 @@
+Jay = int(input("enter age"))
+Viru = int(input("enter age"))
+Gabbar = int(input("enter age"))
+if Jay> Viru :
+    print("Jay is older")
+if Viru> Gabbar:
+    print("Viru is older")
+if  Gabbar> Viru:
+    print("Gabbar is older")
