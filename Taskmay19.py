94411f49f0d3f4ab0e579c3470788cc68cba38df
Mansihub17 <mansirahangdale90@gmail.com>
2026-05-20 12:38:22 +0530

    task on string slicing

---
+++ Taskmay19.py
@@ -0,0 +1,8 @@
+name = "Mansi Rahangdale"
+print(name[0:17:1])
+print(name[-1:-5:-1])
+print(name[16:0:-1])
+print(name[-16:-1:2])
+print(name[10:-1:2])
+print(name[-16:16:3])
+print(name[20:-10:-2])
